455ab84207546026eaf127204f7656740b72d45e
Pawel Morka <morka.paul@gmail.com>
2018-01-28 14:15:48 +0100

    Add index.html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Podaj podstawę trójkąta: '),
+    h = prompt('Podaj wysokość trójkąta: '),
+    triangleArea;
+triangleArea = a * h / 2;
+alert('Pole trójkąta o podstawie a wynoszącej: ' + a + ' i wysokości h wynoszącej: ' + h + ' równa się: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
